0f535e1a3eaee46f953132ebb21365d4be7e733e
Iliyan Kosev <iliyan.v.kosev@gmail.com>
2024-06-30 15:27:50 +0300

    Introduce changes to login test and Base test

---
+++ src/main/java/base/BasePage.java
@@ -0,0 +1,30 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+public class BasePage {
+
+    protected WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void goPage(String url) {
+        driver.get(url);
+    }
+
+    protected void clickOnWebElement(WebElement webElement) {
+        webElement.click();
+    }
+
+    protected void typeInWebElement(WebElement webElement, String text) {
+        webElement.sendKeys(text);
+    }
+
+    public void verifyWebElementIsPresent(WebElement webElement) {
+        Assert.assertTrue(webElement.isDisplayed());
+    }
+}
---
+++ src/main/java/core/App.java
@@ -0,0 +1,30 @@
+package core;
+
+import org.openqa.selenium.WebDriver;
+import pages.DashboardPage;
+import pages.LoginPage;
+
+public class App {
+    private WebDriver driver;
+    private LoginPage loginPage;
+    private DashboardPage dashboardPage;
+
+    public App(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // LAZY Instances
+    public LoginPage loginPage() {
+        if (loginPage == null) {
+            loginPage = new LoginPage(driver);
+        }
+        return loginPage;
+    }
+
+    public DashboardPage dashboardPage() {
+        if (dashboardPage == null) {
+            dashboardPage = new DashboardPage(driver);
+        }
+        return dashboardPage;
+    }
+}
